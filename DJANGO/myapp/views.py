7f71dfe54e0b80e98b3e75c24ff4c842b161ef5d
MilaKonieczna <milkakm2003@gmail.com>
2024-12-05 00:08:11 +0100

    07 List

--- DJANGO/myapp/views.py
+++ DJANGO/myapp/views.py
@@ -1,19 +1,23 @@
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
 from .models import Product, Customer, Order
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
