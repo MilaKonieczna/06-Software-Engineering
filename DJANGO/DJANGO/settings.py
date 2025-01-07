efa12e2a653d89edb55dd222295ce56c07d6d537
MilaKonieczna <milkakm2003@gmail.com>
2025-01-07 19:47:37 +0100

    09 Software Engineering Part 1

--- DJANGO/DJANGO/settings.py
+++ DJANGO/DJANGO/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
