cb4159d5dc05b9580db4d28f28125b9ac617f2f6
MilaKonieczna <milkakm2003@gmail.com>
2025-01-18 22:54:16 +0100

    10.03.17 workflow

--- DJANGO/myapp/models.py
+++ DJANGO/myapp/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=2,
+        decimal_places=5,
         validators=[positive]
     )
     available = models.BooleanField()
